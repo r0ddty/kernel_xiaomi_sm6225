c8e875d0c78b827925f3d46542afcbc0afb4b234
r0ddty <r0d@ukr.net>
2025-02-28 17:45:38 +0200

    fog: use r412851 clang in build script
    Signed-off-by: r0ddty <r0d@ukr.net>

--- kernel_build.sh
+++ kernel_build.sh
@@ -21,7 +21,7 @@ NC='\033[0m'
 if [ ! -e clang-llvm ]
 then
     echo -e "${GREEN}clang not found, cloning Azure clang...${NC}"
-    git clone -b main https://gitlab.com/Panchajanya1999/azure-clang clang-llvm
+    git clone -b r412851 https://github.com/Nicklas373/aosp-clang clang-llvm
 else
     echo -e "${GREEN}clang already present, proceeding...${NC}"
 fi
